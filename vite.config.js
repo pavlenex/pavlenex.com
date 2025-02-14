5fcab8eab865cbd4d236427feaee3ac8e5a8f9b9
Pavlenex <36959754+pavlenex@users.noreply.github.com>
2025-02-14 13:34:23 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/pavlenex-com/',  // Replace with your repository name
   plugins: [react()]
+  publicDir: 'public'
 })
