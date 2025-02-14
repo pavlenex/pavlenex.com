6ee1995a8ad443dc2ee9856a4ea12de3f34dedf2
Pavlenex <36959754+pavlenex@users.noreply.github.com>
2025-02-14 12:52:57 +0100

    Initial commit of website

---
+++ src/app.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ProjectBoard from './components/ProjectBoard'
+
+function App() {
+  return <ProjectBoard />
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '/pavlenex-com/',  // Replace with your repository name
+  plugins: [react()]
+})
